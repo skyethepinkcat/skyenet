9f3f4542f323499346dec9d53b78637209976d13
Skyenet <skye@skyenet.online>
2022-11-28 14:56:50 -0500

    build script added

--- build.bash
+++ build.bash
@@ -6,5 +6,5 @@ buildruby=$(readlink -f build.rb)
 for e in $(find . -name "*.erb");do
   cd $(dirname $e)
   e=$(basename $e)
-  $buildruby $(basename $e) > $(cut -f 1 -d '.' <(echo $e)).html
+  $buildruby $(basename $e) > "$(cut -f 1 -d '.' <(echo $e)).html"
 done
